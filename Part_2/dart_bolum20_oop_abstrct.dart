5cbc1991597f78504420c1b087151983484bfffb
Muhammed Yesilmen <muhammedyesilmen93@gmail.com>
2019-09-05 17:37:45 +0300

    Third Commit

--- Part_2/dart_bolum20_oop_abstrct.dart
+++ Part_2/dart_bolum20_oop_abstrct.dart
@@ -1,3 +1,56 @@
 void main(List<String> args) {
-  
+  Kare kare = Kare();
+  kare.en = 4;
+  kare.boy = 4;
+  int sonuc_K_Alan = kare.alanHesapla();
+  int sonuc_K_Cevre = kare.cevreHesapla();
+
+  Dikdortgen dikdortgen = Dikdortgen();
+  dikdortgen.en = 6;
+  dikdortgen.boy = 10;
+  int sonuc_D_Alan = dikdortgen.alanHesapla();
+  int sonuc_D_Cevre = dikdortgen.cevreHesapla();
+
+  print("""
+  Kare
+  Alan:$sonuc_K_Alan
+  Çevre:$sonuc_K_Cevre
+
+  Dikdörtgen
+  Alan:$sonuc_D_Alan
+  Çevre:$sonuc_D_Cevre
+  """);
+}
+
+abstract class Sekil {
+  int en;
+  int boy;
+
+  int alanHesapla();
+
+  int cevreHesapla();
+}
+
+class Kare extends Sekil {
+  @override
+  int alanHesapla() {
+    return en * boy;
+  }
+
+  @override
+  int cevreHesapla() {
+    return 2 * (en + boy);
+  }
+}
+
+class Dikdortgen extends Sekil {
+  @override
+  int alanHesapla() {
+    return en * boy;
+  }
+
+  @override
+  int cevreHesapla() {
+    return 2 * (en + boy);
+  }
 }
